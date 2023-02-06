4ec2ec4ffd9f390de5377a922f7ecb0f105ddc08
frakic <frakic@frakic.com>
2023-02-06 15:45:54 +0100

    add UserController

---
+++ BitMouse.LeadGenerator.Contract/Users/UserRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BitMouse.LeadGenerator.Contract.Users;
+public class UserRequestDto
+    {
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
