e223ca42242e576b842afc2950274d7585c276af
frakic <frakic@frakic.com>
2023-02-06 16:35:05 +0100

    add fluent validation

---
+++ BitMouse.LeadGenerator.Infrastructure/AspNetCore/Validation/FluentValidationValidatorInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitMouse.LeadGenerator.Infrastructure.AspNetCore.Validation;
+
+public class FluentValidationValidatorInterceptor : IValidatorInterceptor
+{
+    public ValidationResult AfterAspNetValidation(ActionContext actionContext,
+        IValidationContext validationContext,
+        ValidationResult result)
+    {
+        if (!result.IsValid)
+        {
+            throw new ValidationException(result.Errors);
+        }
+
+        return result;
+    }
+
+    public IValidationContext BeforeAspNetValidation(ActionContext actionContext,
+        IValidationContext commonContext)
+    {
+        return commonContext;
+    }
+}
